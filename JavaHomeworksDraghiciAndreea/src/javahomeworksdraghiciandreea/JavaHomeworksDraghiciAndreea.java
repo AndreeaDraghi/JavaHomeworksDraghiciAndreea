196e078e711cf43ac94d9d2e770d81440f70c1a3
Andreea <Andreea@10.0.9.35>
2016-11-10 00:15:22 +0200

    This is the first point of the Homework n. 2

--- JavaHomeworksDraghiciAndreea/src/javahomeworksdraghiciandreea/JavaHomeworksDraghiciAndreea.java
+++ JavaHomeworksDraghiciAndreea/src/javahomeworksdraghiciandreea/JavaHomeworksDraghiciAndreea.java
@@ -1,12 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Homework n. 2
  */
 package javahomeworksdraghiciandreea;
 
+import java.util.Scanner;
 /**
- *
+ * import the Scanner class
  * @author gheorgheaurelpacurar
  */
 public class JavaHomeworksDraghiciAndreea {
@@ -15,7 +14,16 @@ public class JavaHomeworksDraghiciAndreea {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      // Homework 2.1 "short Java program that reads your name from keyboard and writes it in console"
+      
+      System.out.println(" ***** What's my name? *****");
+      
+      Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your first name: " );  
+        System.out.println("Enter your second name: " ); 
+        String FirstName = (scan.nextLine());
+        String SecondName = (scan.nextLine());
+        System.out.println("Your full name is: " + FirstName + " " + SecondName);
     }
     
 }
